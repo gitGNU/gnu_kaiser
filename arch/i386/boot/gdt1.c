333c0fb4eedd8de3da14a344c85d53f3e6a5a9fb
Andrew Etches <seadog@mehmoo.com>
2008-05-28 15:31:23 +0000

    Created the GDT for later use and added some dummy entries

---
+++ arch/i386/boot/gdt1.c
@@ -0,0 +1,44 @@
+/*
+ * gdt.c - GDT initialisation code
+ *
+ * Copyright (C) 2008 Andrew 'Seadog' Etches
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <gdt.h>
+
+gdt_entry_t gdt[3];
+gdt_ptr_t gp;
+
+void gdt_set_gate(int num, unsigned long base, unsigned long limit,
+		  unsigned char access, unsigned char gran){
+	gdt[num].base_low = (base & 0xFFFF);
+	gdt[num].base_middle = (base >> 16) & 0xFF;
+	gdt[num].base_high = (base >> 24) & 0xFF;
+	gdt[num].limit_low = (limit & 0xFFFF);
+	gdt[num].granularity = ((limit >> 16) & 0x0F);
+	gdt[num].granularity |= (gran & 0xF0);
+	gdt[num].access = access;
+}
+
+void gdt_install(){
+	gp.limit = (sizeof(gdt_entry_t) * 3) - 1;
+	gp.base = &gdt;
+	gdt_set_gate(0, 0, 0, 0, 0);
+	gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+	gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+	gdt_flush();
+}
