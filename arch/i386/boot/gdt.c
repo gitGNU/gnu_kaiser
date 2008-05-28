3880c548557bc85b2ece612312c20e301387feb3
Andrew Etches <seadog@mehmoo.com>
2008-05-28 21:49:35 +0000

    Set things up for general load.S and IDT addition

--- arch/i386/boot/gdt.c
+++ arch/i386/boot/gdt.c
@@ -1,5 +1,5 @@
 /*
- * gdt2.S - GDT loading function
+ * gdt.c - GDT initialisation code
  *
  * Copyright (C) 2008 Andrew 'Seadog' Etches
  *
@@ -18,16 +18,27 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-.global gdt_flush
-.extern gp
-gdt_flush:
-	lgdt (gp)
-	mov $0x10, %ax
-	mov %ax, %ds
-	mov %ax, %es
-	mov %ax, %fs
-	mov %ax, %gs
-	mov %ax, %ss
-	jmp flush2
-flush2:
-	ret
+#include <gdt.h>
+
+gdt_entry_t gdt[3];
+gdt_ptr_t gp;
+
+void gdt_set_gate(int num, unsigned long base, unsigned long limit,
+		  unsigned char access, unsigned char gran){
+	gdt[num].base_low = (base & 0xFFFF);
+	gdt[num].base_middle = (base >> 16) & 0xFF;
+	gdt[num].base_high = (base >> 24) & 0xFF;
+	gdt[num].limit_low = (limit & 0xFFFF);
+	gdt[num].granularity = ((limit >> 16) & 0x0F);
+	gdt[num].granularity |= (gran & 0xF0);
+	gdt[num].access = access;
+}
+
+void gdt_install(){
+	gp.limit = (sizeof(gdt_entry_t) * 3) - 1;
+	gp.base = (unsigned int)&gdt;
+	gdt_set_gate(0, 0, 0, 0, 0);
+	gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+	gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+	gdt_flush();
+}
