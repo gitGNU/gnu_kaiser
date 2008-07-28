816c589481744f9377fc96097fcf4f6451df1649
Andrew Etches <seadog@mehmoo.com>
2008-07-28 15:08:44 +0000

    Added kfree declaration to kmalloc.h

--- include/kmalloc.h
+++ include/kmalloc.h
@@ -12,4 +12,6 @@ void *kmalloc_a(uint32_t size); /* returns page aligned pointer */
 void *kmalloc_p(uint32_t size, uint32_t *physical); /*returns both*/
 void *kmalloc_ap(uint32_t size, uint32_t *physical);
 
+void kfree(void *);
+
 #endif
