e76ca75a4fd15093b31c8bbdc5fa80eb2b442041
oohal <oohal@linbox.(none)>
2008-09-28 09:05:12 +1000

    added ringbuffer codez

---
+++ kern/ringbuffer.c
@@ -0,0 +1,78 @@
+/* simple ring buffer implementation */
+#include <stddef.h>
+#include "ringbuffer.h"
+
+size_t ringbuf_write(ringbuf_t *rb, const void *d, size_t len)
+{
+	const uint8_t *data = d;
+	uint8_t *buffer = rb->buffer;
+	size_t i, end = rb->end;
+	
+	if( rb->bytes == rb->size) /* prevent writing into a full buffer*/
+		return 0;
+
+	for(i = 0; i < len && (end != rb->start || rb->bytes == 0); i++, end++) {
+		if( end == rb->size - 1) {
+			end = 0;
+		}
+
+		buffer[end] = data[i];
+	}
+
+	rb->bytes += i;
+	rb->end = end;
+
+	return i;
+}
+
+size_t ringbuf_read(ringbuf_t *rb, void *d, size_t len)
+{
+   	const uint8_t *buffer = rb->buffer;
+	uint8_t *data = d;
+	size_t i, start = rb->start;
+
+	if( rb->bytes == 0) // can't read an empty buffer
+		return 0;
+
+	for(i = 0; i < len && (start != rb->end || rb->bytes == (rb->size - 1)); i++, start++) {
+		if( start == rb->size - 1) {
+			start = 0;
+		}
+
+		data[i] = buffer[start];
+	}
+
+	rb->bytes -= i;
+	rb->start = start;
+	
+	return i;
+}
+
+void ringbuf_delete(ringbuf_t *rb)
+{
+	kfree(rb->buffer);
+	kfree(rb);
+}
+
+ringbuf_t *ringbuf_create(size_t bufsize)
+{
+	ringbuf_t *rb;
+
+	rb = (ringbuf_t *) kmalloc(sizeof(ringbuf_t));
+	if(rb == NULL)
+		return NULL;
+
+	rb->buffer = kmalloc(bufsize);
+	if(rb->buffer == NULL) {
+		free(rb);
+		return NULL;
+	}
+
+	rb->start = 0;
+	rb->bytes = 0;
+	rb->size = bufsize;
+	rb->end = 0;
+	
+	return rb;
+}
+
---
+++ kern/ringbuffer.h
@@ -0,0 +1,23 @@
+#ifndef __RINGBUFFER_H__
+#define __RINGBUFFER_H__
+
+/* simple ring buffer implementation */
+
+#include <stddef.h>
+
+struct ringbuf 
+{
+	size_t size;  /* size of the buffer */
+	size_t bytes; /* numer of bytes currently in the buffer */
+	size_t start, end; /* buffer offsets */
+	void *buffer; /* buffer */
+};
+
+typedef struct ringbuf ringbuf_t;
+
+size_t ringbuf_write(ringbuf_t *rb, const void *d, size_t len);
+size_t ringbuf_read(ringbuf_t *rb, void *d, size_t len);
+void ringbuf_delete(ringbuf_t *rb);
+ringbuf_t *ringbuf_create(size_t bufsize);
+
+#endif
