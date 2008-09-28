37648f97fd1e66e6fa13c13aab4b20add1701ded
Andrew Etches <seadog@mehmoo.com>
2008-09-28 04:58:59 +0000

    Added copyright/license notices to files (why do I never remember beforehand!?)

--- lib/unwind.c
+++ lib/unwind.c
@@ -1,9 +1,29 @@
+/*
+ * unwind.c - unwind the stack to see the chain of function calls
+ *
+ * Copyright (C) 2008 Andrew 'Seadog' Etches
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
 #include <lib/kprintf.h>
 #include <asm/stddef.h>
 #include <lib/unwind.h>
 #include <lib/elf_symtab.h>
 
-/* Unwind the functions called */
 void unwind(void){
 	/* end is defined in our linker script */
   extern uint32_t end;
