3330c445e213352f7c43ed513a6f586f155762e2
Andrew Etches <seadog@mehmoo.com>
2008-09-28 04:45:45 +0000

    Cleaned up variable names

--- lib/elf_symtab.c
+++ lib/elf_symtab.c
@@ -1,9 +1,9 @@
 #include <elf.h>
 #include <lib/elf_symtab.h>
 
-int symbol_table_size;
-Elf32_Sym *symbol_table;
-char *string_table;
+int __symbol_table_size;
+Elf32_Sym *__symbol_table;
+char *__string_table;
 
 void init_symtable(Elf32_Shdr *section_table, int size){
 	Elf32_Shdr *section_header;
@@ -12,11 +12,11 @@ void init_symtable(Elf32_Shdr *section_table, int size){
 	for(i=0; i<size; i++){
 		section_header = &section_table[i];
 		if(section_header->sh_type == SHT_SYMTAB){
-			symbol_table = (Elf32_Sym *)section_header->sh_addr;
-			symbol_table_size = section_header->sh_size / sizeof(Elf32_Sym);
+			__symbol_table = (Elf32_Sym *)section_header->sh_addr;
+			__symbol_table_size = section_header->sh_size / sizeof(Elf32_Sym);
 		} else if(section_header->sh_type == SHT_STRTAB){
-			string_table = kmalloc(section_header->sh_size);
-			string_table = memcpy(string_table, (char *)section_header->sh_addr, section_header->sh_size);
+			__string_table = kmalloc(section_header->sh_size);
+			memcpy(__string_table, (char *)section_header->sh_addr, section_header->sh_size);
 		}
 	}
 }
@@ -26,10 +26,10 @@ char *get_symbol_name(unsigned int symbol_location){
 	Elf32_Sym *symbol;
 	int i;
 
-	for(i=0; i < symbol_table_size; i++){
-		symbol = &symbol_table[i];
+	for(i=0; i < __symbol_table_size; i++){
+		symbol = &__symbol_table[i];
 		if(symbol->st_value == symbol_location){
-			retval = string_table + symbol->st_name;
+			retval = __string_table + symbol->st_name;
 		}
 	}
 	return retval;
